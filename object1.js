bbaa2b6f06c66a51065f42ce59adf0cea7a0f40f
Faridun Eshonov <fariduneshon02@gmail.com>
2024-05-03 14:17:42 +0500

    first commit

---
+++ object1.js
@@ -0,0 +1,110 @@
+// let person = {
+//     firstname: "John",
+//     lastname: "softclub",
+//     age: 19
+// }
+// console.log(person.firstname);
+
+
+
+
+// // let person = {
+// //     firstname: "John",
+// //     lastname: "softclub",
+// //     age: 19
+// // }
+// // console.log(Object.values(person));           // output: [ 'John', 'softclub', 19 ]
+
+
+
+
+// // let person = {
+// //     firstname: "John",
+// //     lastname: "softclub",
+// //     age: 19
+// // }
+// // console.log(Object.keys(person));           // output: [ 'firstname', 'lastname', 'age' ]
+
+
+
+// // let person = {
+// //     firstname: "John",
+// //     lastname: "softclub",
+// //     age: 19
+// // }
+// // console.log(Object.entries(person));           // output: [ [ 'firstname', 'John' ], [ 'lastname', 'softclub' ], [ 'age', 19 ] ]
+
+
+// // let obj = {
+// //     obj2: {
+// //         arr: [1, 2, 3]
+// //     },
+// // };
+// // let { obj2:
+// //     {
+// //         arr: [a, b, c]
+// //     }
+
+// // } = obj
+
+
+// // console.log(b);
+
+
+// //  task 1
+// // function getVoteCount(VOTE) {
+// // return VOTE.upvotes - VOTE.downvotes
+// // }
+// // console.log(getVoteCount({ upvotes: 2, downvotes: 33 }));
+
+
+// //  task 2
+
+
+// // function volumeOfBox(OfBox) {
+// // return  Object.values((OfBox)).reduce((cnt, sum,) => cnt * sum,1)
+// // }
+// // console.log(volumeOfBox({ width: 4, length: 2, height: 2 }));
+
+
+
+// //  task 3
+
+// // function fiftyThirtyTwenty(num) {
+// //     let person = {
+// //         "Needs": (50 * num) / 100,
+// //         "Wants": (30 * num) / 100,
+// //         "Saving": (20 * num) / 100
+
+
+// //     }
+// //     return person
+// // }
+// // console.log(fiftyThirtyTwenty(10000));
+
+
+
+// //  task 4
+
+// function family(name) {
+
+//     let person = {
+//         "Darth Vader": "father",
+//         leia: "sister",
+//         han: "brother in raw",
+//         r2d2: "droid"
+//     }
+//     return `Luke, I am your ${person[name]}`
+
+
+// }
+// console.log(family("leia"));
+
+
+
+//  task 5
+// function cityFacts(country) {
+//     return country.name + " has a population of " + country.population + "  and is situated in " + country.continent
+
+// }
+// console.log(cityFacts({ name: "Paris", population: "2,140,526", continent: "Europe" }));
